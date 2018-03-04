ae11cae210567ff13c3cd4c1053fd2ae6a58309d
Adam Jarzebak <adam@jarzebak.eu>
2018-03-04 19:44:36 +0000

    Add image page and ai page

--- main/app.py
+++ main/app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, Response, jsonify, request
 import settings
-# from camera import VideoCamera
+from camera import VideoCamera
 import time
 import urllib2
+import numpy as np
+import cv2
+import imutils
+import datetime
+
 
 app = Flask(__name__,
             static_url_path='',
@@ -20,6 +25,20 @@ log = settings.logging
 def index():
     return render_template('main.html')
 
+
+@app.route('/vision')
+def visionAnalysis():
+    return render_template('visionAnalysis.html')
+
+
+@app.route('/ai')
+def artificialIntelligence():
+    return render_template('artificialIntelligence.html')
+
+
+
+color = True
+
 @app.route('/_apiQuery')
 def api_query_task():
 
@@ -36,6 +55,145 @@ def api_query_task():
         reply = "Changed to gray"
 
     return jsonify(result=reply)
+
+def gen(camera):
+
+    stream = urllib2.urlopen("http://68958932.ngrok.io/stream.mjpg")
+    bytes = ''
+
+    while True:
+
+        bytes += stream.read(1024)
+        a = bytes.find('\xff\xd8')
+        b = bytes.find('\xff\xd9')
+        if a != -1 and b != -1:
+            frameBytes = bytes[a:b + 2]
+            bytes = bytes[b + 2:]
+
+            img = camera.makeImage(frameBytes, color, False)
+            # image = camera.bytesToImage(frameBytes)
+
+            # img = camera.findFaces(frameBytes)
+
+            # canny = camera.changeToGray(image)
+
+            # frameBytes = camera.imageToBytes(canny)
+
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n\r\n')
+
+            time.sleep(0.00001)
+
+
+def detectMotion():
+    stream = None
+
+    try:
+        stream = urllib2.urlopen("http://68958932.ngrok.io/stream.mjpg")
+        log.info("Successfully opened stream")
+    except urllib2.HTTPError as e:
+        code = e.code
+        log.error("URLLIB eror: %s" %code)
+
+    bytes = ''
+
+    firstFrame = None
+
+    if stream != None:
+        while True:
+
+            bytes += stream.read(1024)
+            a = bytes.find('\xff\xd8')
+            b = bytes.find('\xff\xd9')
+            if a != -1 and b != -1:
+                frameBytes = bytes[a:b + 2]
+                bytes = bytes[b + 2:]
+
+                nparr = np.fromstring(frameBytes, np.uint8)
+
+                frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+
+                # grab the current frame and initialize the occupied/unoccupied
+                # text
+                # (grabbed, frame) = camera.read()
+                text = "Unoccupied"
+                #
+                # # if the frame could not be grabbed, then we have reached the end
+                # # of the video
+                #
+                # if not grabbed:
+                #     break
+
+                # # resize the frame, convert it to grayscale, and blur it
+                # frame = imutils.resize(frame, width=500)
+                frame = cv2.resize(frame, (640, 480))
+
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                gray = cv2.GaussianBlur(gray, (21, 21), 0)
+                #
+                # # if the first frame is None, initialize it
+                if firstFrame is None:
+                    firstFrame = gray
+                    continue
+
+                frameDelta = cv2.absdiff(firstFrame, gray)
+                thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+                # dilate the thresholded image to fill in holes, then find contours
+                # on thresholded image
+                thresh = cv2.dilate(thresh, None, iterations=2)
+                (cnts, _, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                             cv2.CHAIN_APPROX_SIMPLE)
+
+                # loop over the contours
+                for c in cnts:
+                    # if the contour is too small, ignore it
+                    # print(len(cnts))
+                    pass
+
+                    # print (cv2.contourArea(c))
+                    # if cv2.contourArea(c) < args["min_area"]:
+                    #     continue
+
+                    # compute the bounding box for the contour, draw it on the frame,
+                    # and update the text
+                    # (x, y, w, h) = cv2.boundingRect(c)
+                    # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    # text = "Occupied"
+
+                # draw the text and timestamp on the frame
+                cv2.putText(frame, "Status: {}".format(text), (10, 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+                            (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+
+                ret, imageJPG = cv2.imencode('.jpg', frame)
+
+
+                toSend = imageJPG.tobytes()
+
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + toSend + b'\r\n\r\n')
+
+                time.sleep(0.00001)
+
+    else:
+        pass
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')\
+
+
+@app.route('/motion_detection')
+def motion_detection():
+    return Response(detectMotion(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, threaded=True, debug=True)
---
+++ main/camera.py
@@ -0,0 +1,103 @@
+import settings
+import cv2
+import numpy as np
+
+log = settings.logging
+
+class VideoCamera(object):
+    def __init__(self):
+        # self.video = cv2.VideoCapture()
+        # self.video = cv2.VideoCapture('http://10.2.254.43:5000/video_feed')
+        # frame = cv2.resize(frame, (640, 480))
+
+        # self.video.set(3, 640)
+        # self.video.set(4, 480)
+
+        log.info("Initialized video camera")
+
+    def __del__(self):
+        pass
+        # self.video.release()
+
+    def cannyStream(self, frame):
+        edges = cv2.Canny(frame, 200, 300)
+
+        return edges
+
+
+    def bytesToImage(self, bytes, color=False):
+
+        nparr = np.fromstring(bytes, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        if not color:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        ret, image = cv2.imencode('.jpg', img)
+
+        return image
+
+
+    def findFaces(self, bytes):
+
+        nparr = np.fromstring(bytes, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        ret, frame = cv2.imencode('.jpg', img)
+
+        cascPath = "haarcascade_frontalface_default.xml"
+        faceCascade = cv2.CascadeClassifier(cascPath)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        # print (len(faces))
+        if (len(faces) > 0):
+            print("Found: {}".format(len(faces)))
+        else:
+            print("Not found:")
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        return frame
+
+
+    def makeImage(self, bytes, color=True, backSub=False):
+        nparr = np.fromstring(bytes, np.uint8)
+
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        if backSub:
+            fgbg = cv2.createBackgroundSubtractorMOG2()
+            image = fgbg.apply(image)
+
+            ret, jpeg = cv2.imencode('.jpg', image)
+
+            return jpeg.tobytes()
+
+
+        if not color:
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+            ret, jpeg = cv2.imencode('.jpg', gray)
+
+            return jpeg.tobytes()
+
+        else:
+            ret, jpeg = cv2.imencode('.jpg', image)
+
+            return jpeg.tobytes()
+
+
+    def imageToBytes(self, image):
+
+        return image.tobytes()
